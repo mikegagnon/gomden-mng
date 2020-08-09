b83f35c907a5704d73123cdf4a925be3d529a623
Michael Gagnon <mikegagnon@gmail.com>
2020-08-08 20:08:20 -0400

    new tables for pages

--- gomden.sql
+++ gomden.sql
@@ -23,9 +23,33 @@ CREATE TABLE "usedtokens" (
     "ts" BIGINT DEFAULT CAST((extract(epoch from now()) * 1000) as BIGINT) /* num milliseconds since epoch */
 );
 
+CREATE TABLE "pages" (
+    "pageid" BIGSERIAL PRIMARY KEY,
+    "contributoruserid" BIGINT, /* the author of this revision */
+    "pagename" TEXT,
+    "revision" BIGINT, /* each version of a page gets its own row. revision # distinguishes between the various versoins */
+    "content" TEXT,
+    "ts" BIGINT DEFAULT CAST((extract(epoch from now()) * 1000) as BIGINT) /* num milliseconds since epoch */
+);
+
+CREATE TABLE "pagepermissions" (
+    "pagepermissionid" BIGSERIAL PRIMARY KEY,
+    "pagename" TEXT,
+    "owneruserid" BIGINT,   /* the creator of the page */
+    "allowcomments" BIGINT, /* 0 if not allowed; 1 if allowed */
+    "allowedits" BIGINT,    /* 0 if not allowed; 1 if allowed */
+    "ts" BIGINT DEFAULT CAST((extract(epoch from now()) * 1000) as BIGINT) /* num milliseconds since epoch */
+);
+
 # Upon setting up the site, the first user to create an acccount is granted the
 # ROOT and ADMIN roles
 INSERT INTO roles (userid, role) VALUES (1, '*ROOT*');
 INSERT INTO roles (userid, role) VALUES (1, '*ADMIN*');
+
+INSERT INTO pages (contributoruserid, pagename, revision, content)
+VALUES (1, 'home', 1, 'Hello.');
+
+INSERT INTO pagepermissions (pagename, owneruserid, allowcomments, allowedits)
+VALUES ('home', 1, 1, 1);
 
 COMMIT;
